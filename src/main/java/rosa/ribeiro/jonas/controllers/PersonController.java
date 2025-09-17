376c13c6e0cecfa63d371ff5e216d9c0d2a61b7b
Jonas Ribeiro <jonas.ribeiro@rodipa.com.br>
2025-09-17 13:43:12 -0300

    Estruturando endpoint person, e fazendo um mock de PersonModel

--- src/main/java/rosa/ribeiro/jonas/controllers/PersonController.java
+++ src/main/java/rosa/ribeiro/jonas/controllers/PersonController.java
@@ -1,7 +1,28 @@
 package rosa.ribeiro.jonas.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import rosa.ribeiro.jonas.model.PersonModel;
+import rosa.ribeiro.jonas.services.PersonService;
+
+import java.util.logging.Logger;
 
 @RestController
+@RequestMapping("/person")
 public class PersonController {
+
+    @Autowired
+    private PersonService service;
+
+    @RequestMapping(value = "/{id}",
+        method = RequestMethod.GET,
+        produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public PersonModel findById(@PathVariable("id") String id){
+        return service.findById(id);
+    }
 }
--- src/main/java/rosa/ribeiro/jonas/services/PersonService.java
+++ src/main/java/rosa/ribeiro/jonas/services/PersonService.java
@@ -1,4 +1,26 @@
 package rosa.ribeiro.jonas.services;
 
+import org.springframework.stereotype.Service;
+import rosa.ribeiro.jonas.model.PersonModel;
+
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.logging.Logger;
+
+@Service
 public class PersonService {
+
+    private final AtomicLong counter = new AtomicLong();
+    private Logger logger = Logger.getLogger(PersonService.class.getName());
+
+    public PersonModel findById(String id){
+        logger.info("Finding one Person!");
+        PersonModel person = new PersonModel();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("Leandro");
+        person.setLastName("Silva");
+        person.setAddress("Uberlândia-MG");
+        person.setGender("Male");
+        return person;
+    }
+
 }
